59bd849ecf752d8b792f93a7413295bc2186f1c3
Laion Ubert <laiondevubert@gmail.com>
2024-07-21 23:05:42 -0300

    Primeiro Commit!

---
+++ deleteScript.py
@@ -0,0 +1,21 @@
+"""Script para deletar arquivos de pasta especifica a cada N dias"""
+import os, time, sys
+from pathlib import Path
+
+MAIN_DIR = Path(__file__).parent #Define diretório local do aplicativo
+TIME_FILE = MAIN_DIR / 'config/time.txt' #Aponta o arquivo com a informação
+PATH_FILE = MAIN_DIR / 'config/path.txt' #Aponta o arquivo com o diretório a ser manipulado
+
+timeNow = time.time()
+
+configPath = open(PATH_FILE)
+configTime = open(TIME_FILE)
+contentPath = configPath.read()
+contentTime = int(configTime.read())
+
+print(f"O Diretório é: {contentPath}")
+print(f"O Tempo de Deleção é: {timeNow}")
+print(f"O Novo tempo é: {((timeNow) - contentTime)}")
+
+configPath.close
+configPath.close
