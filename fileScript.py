6712a88b2eecb38e5b99a20a7e7a94cc858d2f99
Laion Ubert <laiondevubert@gmail.com>
2024-08-06 10:12:33 -0300

    Interface Gráfica Finalizada

--- fileScript.py
+++ fileScript.py
@@ -5,35 +5,35 @@
 '''
 
 
-#Importacao de modulos e libs
+# Importacao de modulos e libs
 import os, datetime, shutil, pathlib
 import configparser
 from datetime import datetime, timedelta
 
 
-#Inicializacao do configparser
+# Inicializacao do configparser
 config = configparser.ConfigParser()
 configFile = r'config.ini'
 config.read(configFile)
 
 
-#Modificacao das configuracoes
-##getters dos valores de input para as variaveis de mudanca
+# Modificacao das configuracoes
+## getters dos valores de input para as variaveis de mudanca
 config_path = str(config.get('Default','config_path'))
 config_path_copy = str(config.get('Default','config_path_copy'))
 config_time = int(config.get('Default','config_time'))
 selfDir = os.path.dirname(__file__)
 
-##alterar idade maxima dos arquivos
+## alterar idade maxima dos arquivos
 timeDelete = datetime.now() - timedelta(days = config_time)
 
-##impressao dos valores atualizados das variaveis 
-print(f"Diretório onde serão deletados os arquivos  :  {config_path}") #Imprime no terminal o caminho do diretorio
-print(f"Serão deletados os arquivos anteriores a    :  {timeDelete}") #Imprime no terminal a data limite para deletar arquivos
+## impressao dos valores atualizados das variaveis 
+print(f"Diretório onde serão deletados os arquivos  :  {config_path}") # Imprime no terminal o caminho do diretorio
+print(f"Serão deletados os arquivos anteriores a    :  {timeDelete}") # Imprime no terminal a data limite para deletar arquivos
 
 
-#Funcoes
-##funcao para listar arquivos a serem deletados
+# Funcoes
+## funcao para listar arquivos a serem deletados
 def listarArquivos(path = os.chdir(os.path.join(os.getcwd(), config_path_copy))):
     with os.scandir(path) as entries:
         try:
@@ -45,7 +45,7 @@ def listarArquivos(path = os.chdir(os.path.join(os.getcwd(), config_path_copy)))
         except PermissionError:
             print(f'Arquivo com acesso negado: {path}')
 
-##funcao para deletar arquivos
+## funcao para deletar arquivos
 def deletarArquivos():
     if os.path.exists(config_path) == True:
         os.chdir(os.path.join(os.getcwd(), config_path))
@@ -57,7 +57,7 @@ def deletarArquivos():
     else:
         print("Diretório de destino não acessível. Favor verificar se o caminho existe e está disponível para Leitura/Escrita")
 
-##funcao para copiar arquivos
+## funcao para copiar arquivos
 def copiarArquivos():
     if os.path.exists(config_path) and os.path.exists(config_path_copy) == True:
         os.chdir(os.path.join(os.getcwd(), config_path))
@@ -71,8 +71,8 @@ def copiarArquivos():
         print("Diretório de origem não acessível. Favor verificar se o caminho existe e está disponível para Leitura/Escrita")
 
 
-#listarArquivos()
-#Execucao das funcoes com base na diferenca de tempo
+# listarArquivos()
+# Execucao das funcoes com base na diferenca de tempo
 if datetime.now() > timeDelete:
     if config.getboolean('Tweaks','option_deletar_arquivos') == 1:
         deletarArquivos()
